054248405dfe149415885e504a1d36d3e25cdec5
flyelmos <emo_55@abv.bg>
2017-01-18 21:22:49 +0200

    More projects created

---
+++ Data Types and Variables Lab exercises/Circle Area (12 digits precision)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Area__12_digits_precision_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a radius: ");
+            double r = double.Parse(Console.ReadLine());
+            double area = Math.PI * r * r;
+            Console.WriteLine("Area of the circle is {0:f12}",area);
+        }
+    }
+}
---
+++ Data Types and Variables Lab exercises/Special Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int num = 0; num <= n; num++)
+            {
+                 int sum = 0;
+                int digits = num;
+
+                while (digits > 0)
+                {
+                    sum += digits % 10;
+                    digits = digits / 10;
+                }
+
+                bool answer = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}",num, answer);
+
+            }
+        }
+    }
+}
